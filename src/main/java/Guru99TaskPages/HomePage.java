1071d30c23c09633abd42e6c29cb7e31ba6b6f30
nehallxx <132005828+nehallxx@users.noreply.github.com>
2025-01-09 01:54:53 +0200

    Update HomePage.java

--- src/main/java/Guru99TaskPages/HomePage.java
+++ src/main/java/Guru99TaskPages/HomePage.java
@@ -1,4 +1,21 @@
-package Guru99TaskPages;
+package TestsDir;
 
-public class HomePage {
+import Guru99TaskPages.MobilePage;
+import org.testng.annotations.Test;
+import static org.testng.Assert.assertTrue;
+public class HomePageTest extends TaskSetup{
+
+    @Test
+    public void HomePageTitleTest(){
+        assertTrue(homePage.isTitleVisible());
+    }
+    @Test
+    public void MobilePageTitleTest(){
+        MobilePage mobilePage=homePage.clickMobileOption();
+        assertTrue(mobilePage.isTitleVisible());
+        mobilePage.clickDropdown("http://live.techpanda.org/index.php/mobile.html?dir=asc&order=name");
+        assertTrue(mobilePage.isSortByNameSelected());
+    }
+
 }
+
